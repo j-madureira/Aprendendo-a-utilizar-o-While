2ead5239e91bde39c20a01ddd7f403694c9b9cd7
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-04-14 16:42:40 -0300

    Add files via upload

---
+++ while.js
@@ -0,0 +1,9 @@
+/*Criar uma variável do tipo Number com o valor 1
+  Criar um while para se repetir 24 vezes
+*/
+
+var numero = 1;
+while(numero < 24){
+  console.log(`Esse while está se repetindo pela ${numero} vez`);
+  ++numero;
+}
